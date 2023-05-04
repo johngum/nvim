0a05d43c4d78a70cf5d13a6d1fa8f7c4293d13b8
John Gum <gumesheimer@gmail.com>
2023-05-04 23:41:44 +0200

    more keymaps

--- init.lua
+++ init.lua
@@ -9,14 +9,22 @@ local remap = vim.api.nvim_set_keymap
 
 remap("", "<Space>", "<Nop>", opts)
 
--- Move text up and down
-remap("n", "<A-j>", ":m .+1<CR>==", opts)
-remap("n", "<A-k>", ":m .-2<CR>==", opts)
+-- Navigation
+remap("n", "<C-d>", "<C-d>zz", opts)
+remap("n", "<C-u>", "<C-u>zz", opts)
+remap("n", "n", "nzz", opts)
+remap("n", "N", "Nzz", opts)
 
-remap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-remap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+-- Yank/Paste
+remap("n", "<leader>y", [["+y]], opts)
+remap("v", "<leader>y", [["+y]], opts)
+remap("n", "<leader>Y", [["+Y]], opts)
+remap("v", "<leader>Y", [["+Y]], opts)
+remap("x", "<leader>p", [["_dP]], opts)
+remap("n", "<leader>d", [["_d]], opts)
 
-remap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-remap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+-- Move text up and down only in visual mode
+remap("v", "J", ":m '>+1<CR>gv=gv", opts)
+remap("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 remap("i", "<C-c>", "<ESC>", opts)
